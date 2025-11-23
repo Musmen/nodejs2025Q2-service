bee48e46e0e1d432e401584683cef025576a082f
Musmen <musmen@internet.ru>
2025-11-23 22:41:30 +0300

    feat: setup prisma, docker-compose with user network and setup restarting upon changes implemented into src folder

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
 FROM node:24.11.1-alpine3.22
 WORKDIR /app
+
 COPY package*.json .
 COPY tsconfig*.json .
-COPY . .
-# RUN npm install -g @nestjs/cli
+COPY prisma ./prisma/
+
+RUN npm install -g @nestjs/cli
 RUN npm ci --only=production && \
     npm cache clean --force && \
     rm -rf /tmp/* /var/tmp/*
-CMD ["npm", "start"]
+
+RUN npm install -g nodemon
+
+# COPY . .
+
+CMD sh -c "npx prisma migrate dev --name init && npx prisma generate && npm run start:nodemon"
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 
+import { prisma } from '../prisma/prisma';
+
 import { AppModule } from './app.module';
 
 const DEFAULT_PORT = 4000;
@@ -13,4 +15,25 @@ async function bootstrap() {
 
   await app.listen(port);
 }
+
 bootstrap();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      login: 'Alice' + Date.now().toString(),
+      password: 'alice@prisma.io',
+      version: 1,
+      createdAt: Date.now().toString(),
+      updatedAt: '0',
+    },
+  });
+  console.log('Created user:', user);
+
+  const allUsers = await prisma.user.findMany();
+  console.log('All users:', JSON.stringify(allUsers, null, 2));
+}
+
+main().then(() => {
+  console.log('main is Ok!');
+});
