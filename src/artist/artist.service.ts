3e74f8b998d4c715a557ef551f68ad3c530cc9b6
Musmen <musmen@internet.ru>
2025-11-25 18:17:09 +0300

    feat: Artists data is stored in PostgreSQL database and prisma interacts with the database to manipulate data

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
-import { artistDB } from './db/artist.db';
+import { prisma } from 'prisma/prisma';
 
 import type { Artist } from 'src/interfaces/artist.interface';
 
 @Injectable()
 export class ArtistService {
-  getAllArtists = async () => artistDB.getAllArtists();
+  getAllArtists = async (): Promise<Artist[]> => await prisma.artist.findMany();
 
   getArtistById = async (id: string): Promise<Artist | null> =>
-    artistDB.getArtistById(id);
+    await prisma.artist.findUnique({ where: { id } });
 
-  createArtist = async (newArtistDto: Omit<Artist, 'id'>) =>
-    artistDB.createArtist(newArtistDto);
+  createArtist = async (newArtistDto: Omit<Artist, 'id'>): Promise<Artist> =>
+    await prisma.artist.create({ data: newArtistDto });
 
   updateArtist = async (
-    currentArtist: Artist,
+    { id }: Artist,
     updateArtistDto: Omit<Artist, 'id'>,
-  ) => artistDB.updateArtist(currentArtist, updateArtistDto);
+  ): Promise<Artist> =>
+    await prisma.artist.update({ where: { id }, data: updateArtistDto });
 
-  deleteArtist = async (currentArtist: Artist) =>
-    artistDB.deleteArtist(currentArtist);
+  deleteArtist = async ({ id }: Artist): Promise<void> => {
+    await prisma.artist.delete({ where: { id } });
+  };
 }
